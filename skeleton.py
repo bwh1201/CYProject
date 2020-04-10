031196e6e1e5f9fa5ff9dd52abf8b535682efd5d
bwh1201 <63434207+bwh1201@users.noreply.github.com>
2020-04-10 09:36:07 -0400

    Create skeleton.py

---
+++ skeleton.py
@@ -0,0 +1,59 @@
+'''
+Name: Nick Ashby and Blake Havern
+Project Skeleton: Cow Loan Projection
+
+Description:
+The product that we wish to provide is a simulator that will allow cadets to practice their 
+financial habits before they take out their Cow Loan. In this simulator, users will not only
+ be able to choose what to spend their money on (car, class ring, engagement ring, etc), but
+ also see what may happen to their money if they choose to put it in the stock market. During 
+ their time as cadets, major events will take place that can influence what happens to their money.
+ This simulator will allow Cadets to see the projected results of their financial habits 10, 20, 
+ and 30 years after they receive their Cow Loan. The project will operate like a game, using the 
+ pygame zero library which will help create the graphic user interface.  The game will present 
+ options for portions of the cow loan and then will simulate a potential future as a result of 
+ these decisions. Using pygame zero will allow images and user control over the decisions being taken and 
+ increase the aesthetic of the game.  Overall, the creation of this project will be entirely 
+ in python using the skills we have learned from the course.  The final product will yield an
+ entertaining and informative way for cadets to maximize their cow loan. 
+
+
+Things we need: clever name, image of cadet, backgrounds, 
+'''
+
+#Global Constants- main character, background, colros, bank
+WIDTH = 500
+HEIGHT = 500
+
+GRAY = (138,138,138)
+
+cadet = Actor('cadet')
+cadet.pos = (WIDTH//3. HEIGHT//3)
+
+
+
+
+
+
+#Variables:
+
+#Cadet-> stored as a list, [name, alive(bool), money(nested list with where it is?), frugal score, karma]
+
+#Timeline -> nested list shows where the cadet is at in the story
+
+#Weekends -> Not sure how to make this a variable
+
+#Major events -> Random potential events that could increase money or decrease it by large amounts
+
+#Money management -> 
+
+#General timeline-> opening screen pick character name, select mentors,  make initial financial decisions, 
+#first free weekend,  major event 1(good or bad), remake selections, army-navy occurs, winter break plans, 
+#class weekend, major event #2, summer plans
+
+
+
+
+
+
+
